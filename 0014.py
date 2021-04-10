db256853192deb77a9478b885249a08f40a27279
spankyf <deanflanagan@gmail.com>
2021-04-09 20:13:15 -0400

    Finished 0014.py

--- 0014.py
+++ 0014.py
@@ -1,6 +1,24 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        return 'answer'
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+
+        if len(strs) == 0:
+            return ""
+        else:
+            shortest_word = min(strs, key = len)
+
+            for ind in reversed(range(1,len(shortest_word)+1)):
+                if all([y.startswith(shortest_word[:ind]) for y in strs ]):
+                    return shortest_word[:ind]
+            return ""
 
 x = Solution()
-x.longestCommonPrefix(["flower","flow","flight"])
+print(x.longestCommonPrefix(strs=["flower","flow","flight"]))
+print(x.longestCommonPrefix(strs=[]))
+print(x.longestCommonPrefix(strs=["ft"]))
+
+# x = ["flower","flow","flight"]
+
+
+
+
+        
