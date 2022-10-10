0b421ed86de799eacd6fcdf53f1290b079699eb8
spankyf <deanflanagan@gmail.com>
2022-10-09 21:04:07 -0300

    Rearranged dirs

--- data_builder.py
+++ data_builder.py
@@ -1,21 +1,24 @@
-import pandas as pd
-import os
-import json
+import os, json
 
 ref_file = os.path.join(os.getcwd(),'react-test/src','data.json')
 solutions = os.path.join(os.getcwd(), 'solutions')
 git_root_path = 'https://github.com/deanflanagan/leetcode/tree/master/'
 
 metadata_dict = []
+sort_id = 1 # used for react-data-table-component for unique id
 for dirs,_,files in os.walk(solutions):
     problem_id = dirs.split('/')[-1].lstrip('0')
-    if 'metadata.json' in files:
+    if 'metadata.json' in files: # I only add this after solution accepted on leetcode site
         cur_path = os.path.join(solutions, dirs, [x for x in files if 'metadata.json' in files][0])
         with open(cur_path, 'r') as f:
-            prob_data = json.load(f)
-            prob_data['id'] = problem_id
-            prob_data['git'] = git_root_path + dirs.split('leetcode/')[1]
-            # print(prob_data)
-            metadata_dict.append(prob_data)
+            # have to eval metadata as array, not single object
+            print('found',dirs)
+            solution_array = json.load(f)
+            for prob_data in solution_array:
+                prob_data['number'] = problem_id
+                prob_data['git'] = git_root_path + dirs.split('leetcode/')[1]
+                prob_data['id'] = sort_id
+                sort_id += 1
+                metadata_dict.append(prob_data)
 with open(ref_file, "w") as final:
    json.dump(metadata_dict, final)
